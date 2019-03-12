44ff6a0eee8b746934e7b6cbbdd95e1372808441
dkoderdvl <dkoder.dvl@gmail.com>
2019-03-12 13:42:00 +0300

    add db & table products & seed pizza

--- Gemfile
+++ Gemfile
@@ -2,4 +2,11 @@ source "https://rubygems.org"
 
 gem "sinatra"
 gem "sinatra-reloader"
-gem "active-record"
+gem "sqlite3", '~> 1.3.6'
+gem "activerecord"
+gem "sinatra-activerecord"
+gem "sinatra-contrib"
+
+group :development do
+  gem "tux"
+end
--- public/app.js
+++ public/app.js
@@ -1,4 +1,12 @@
 function something() {
-    alert('Hi dkoder')
+  var x = window.localStorage.getItem('aaa');
+  x = x * 1 + 1; // преобразование x к типу число
+  window.localStorage.setItem('aaa', x);
+  alert(x);
+}
+
+function add-cart() {
+  var cart = window.localStorage.getItem('cart');
+
 }
 
